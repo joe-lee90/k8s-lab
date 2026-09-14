456c9e6c0673d6b4334fbba6f06cb556ba046c6c
Joe Lee <joe.lee.code@gmail.com>
2026-09-14 11:48:42 -0700

    Stage 2: Docker

--- app/app.py
+++ app/app.py
@@ -64,7 +64,7 @@ async def lifespan(app: FastAPI):
     await app.state.redis.aclose()
 
 
-app = FastAPI(title=APP_NAME, version="0.1.0", lifespan=lifespan)
+app = FastAPI(title=APP_NAME, version="0.1.1", lifespan=lifespan)
 
 
 class ValueIn(BaseModel):
